8d5b0a6d4a1626e6e8dab4d973a0bb8c6a10e9bb
MarceloRPC <marcelo.paiva@argobr.com>
2025-02-20 16:04:55 -0300

    base project structure

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <>
-      <h1>Rocket Notes</h1>
-    </>
-  )
-}
-
-export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+import { Details } from './pages/Details/index.jsx';
+import theme from "./styles/theme.js";
+import GlobalStyles from "./styles/global.js";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Details />
+    </ThemeProvider>
   </StrictMode>,
-)
+);
